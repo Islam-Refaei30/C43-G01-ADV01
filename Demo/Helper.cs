e2b9e9d12cc6f67cee334d2cea587c3dc831c0a9
islam-refaei <i.refaei.refaei.saadawi@gmail.com>
2025-03-03 23:27:43 +0200

    Part 01 Swap Example

---
+++ Demo/Helper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+	internal class Helper
+	{
+		#region SWAP Method
+		#region Befor Genenrics
+		//public static void SWAP(ref int x, ref int y)
+		//{
+		//	int temp = x;
+		//	x = y;
+		//	y = temp;
+		//}
+
+
+		//public static void SWAP(ref double x, ref double y)
+		//{
+		//	double temp = x;
+		//	x = y;
+		//	y = temp;
+		//}
+
+		//public static void SWAP(ref Point x, ref Point y)
+		//{
+		//	Point temp = x;
+		//	x = y;
+		//	y = temp;
+		//}
+
+		//public static void SWAP(ref object x, ref object y)
+		//{
+		//	object temp = x;
+		//	x = y;
+		//	y = temp;
+		//}
+		#endregion
+
+		#region After Generics
+		//public static void SWAP<T>(ref T x, ref T y)
+		//{
+		//	T temp = x;
+		//	x = y;
+		//	y = temp;
+		//}
+		
+		// Compiler can Detect the type of T based on the typr op "Method input parameters"
+		// In case Generics type T is declered on method level, Not [class - struct - interface] Level
+		#endregion
+		#endregion
+
+
+	}
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,46 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			#region Part 01 Swap Example
+
+			#region Befor Genenrics
+			//int A = 5, B = 8;
+			//double A = 55, B = 88;
+			//Console.WriteLine($"A = {A}, B = {B} Befor Swap");
+			//Helper.SWAP(ref A, ref B);
+			//Console.WriteLine($"A = {A}, B = {B} After Swap");
+
+			//Point p1 = new Point(50, 30);
+			//Point p2 = new Point(20, 10);
+
+			//object p1 = new Point(50, 30);
+			//object p2 = new Point(20, 10);
+
+			//Console.WriteLine($"{p1}\n{p2}\nBefor Swap\n");
+			//Helper.SWAP(ref p1, ref p2);
+			//Console.WriteLine($"{p1}\n{p2}\nAfter Swap");
+			#endregion
+
+			#region After Genenrics
+			//int A = 5, B = 8;
+			//double A = 55, B = 88;
+
+			//Console.WriteLine($"A = {A}, B = {B} Befor Swap");
+			//Helper.SWAP<double>(ref A, ref B);
+			//Console.WriteLine($"A = {A}, B = {B} After Swap");
+
+			//Point p1 = new Point(50, 30);
+			//Point p2 = new Point(20, 10);
+
+			//object p1 = new Point(50, 30);
+			//object p2 = new Point(20, 10);
+
+			//Console.WriteLine($"{p1}\n{p2}\nBefor Swap\n");
+			//Helper.SWAP(ref p1, ref p2);
+			//Console.WriteLine($"{p1}\n{p2}\nAfter Swap");
+			#endregion
+
+			#endregion
 		}
 	}
 }
