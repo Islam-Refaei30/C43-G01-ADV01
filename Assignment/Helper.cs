bd82a165fc3e99eb84099dd074c7468aa6fab554
islam-refaei <i.refaei.refaei.saadawi@gmail.com>
2025-03-05 01:53:51 +0200

    BubbleSort

---
+++ Assignment/Helper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+	internal class Helper<T> where T : IComparable<T>
+	{
+		public static void SWAP<T>(ref T x, ref T y) 
+		{
+			T temp = x;
+			x = y;
+			y = temp;
+		}
+
+		public static void BubbleSort(ref T[] array)
+		{
+			if (array is not null)
+			{
+				bool swapped;
+				for (int i = 0; i < array.Length - 1; i++)
+				{
+					swapped = false;
+					for (int j = 0; j < array.Length - i - 1; j++)
+					{
+						if (array[j].CompareTo(array[j + 1]) > 0)
+						{
+							SWAP(ref array[j], ref array[j + 1]);
+							swapped = true; 
+						}
+					}
+
+					if (!swapped)
+						break;
+				}
+			}
+		}
+
+
+	}
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+			int[] numbers = { 5, 3, 8, 1, 2 };
+
+			Console.WriteLine($"Befor:\n");
+			foreach (int i in numbers)
+			{
+				Console.Write(i+" ");
+			}
+	
+
+			Helper<int>.BubbleSort(ref numbers);
+
+			Console.WriteLine($"\n\nAfter:\n");
+			foreach (int i in numbers) 
+			{
+				Console.Write(i+" ");
+			}
+		}
     }
 }
