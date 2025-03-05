e8e76d48d30e920784b6db2ea9cf2edf1cc68f70
islam-refaei <i.refaei.refaei.saadawi@gmail.com>
2025-03-05 14:39:38 +0200

    Q2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,22 +4,33 @@
     {
         static void Main(string[] args)
         {
-			int[] numbers = { 5, 3, 8, 1, 2 };
+			#region Q1
+			//int[] numbers = { 5, 3, 8, 1, 2 };
 
-			Console.WriteLine($"Befor:\n");
-			foreach (int i in numbers)
-			{
-				Console.Write(i+" ");
-			}
-	
+			//Console.WriteLine($"Befor:\n");
+			//foreach (int i in numbers)
+			//{
+			//	Console.Write(i + " ");
+			//}
 
-			Helper<int>.BubbleSort(ref numbers);
 
-			Console.WriteLine($"\n\nAfter:\n");
-			foreach (int i in numbers) 
-			{
-				Console.Write(i+" ");
-			}
+			//Helper<int>.BubbleSort(ref numbers);
+
+			//Console.WriteLine($"\n\nAfter:\n");
+			//foreach (int i in numbers)
+			//{
+			//	Console.Write(i + " ");
+			//}
+			#endregion
+
+			#region Q2
+			var Range = new Range<int>(60, 60);
+			Console.WriteLine($"15 in rand? {Range.IsInRange(15)}");
+			Console.WriteLine($"Length =  {Range.Length()}");
+
+			#endregion
+
+
 		}
     }
 }
---
+++ Assignment/Range.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+	internal class Range<T> where T : IComparable<T>
+	{
+		public T Min { get; private set; }
+		public T Max { get; private set; }
+
+		public Range(T min, T max)
+		{
+			if (min.CompareTo(max) > 0)
+			{
+				Max = min;
+				Min = max;
+			}
+			else
+			{
+				Min = min;
+				Max = max;
+			}
+		}
+
+		public bool IsInRange(T value)
+		{
+			if (value is not null)
+			return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
+			return false;
+		}
+
+		public dynamic Length()
+		{
+			try
+			{
+				return (dynamic)Max - (dynamic)Min;
+			}
+			catch (Exception ex)
+			{
+				throw new InvalidOperationException($"Operation not supported for type {typeof(T)}", ex);
+			}
+		}
+	}
+}
